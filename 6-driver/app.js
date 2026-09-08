c392cdc649d769e22eb5166843d1c73d80534d07
Vyacheslav <ssllav@gmail.com>
2026-09-08 12:07:41 +0300

    add 06
    # Conflicts:
    #	6-driver/app.js

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -1,1 +1,7 @@
+// Вариант для истинных фанатов короткого замыкания - метод "сквозняка" без сбора логики
 const result = (age >= 18 && hasLicence && !isDrunk) && 'может' || 'не может';
+console.log(result);
+
+// вариант вероятно как положено, через тернарный
+const canDrive = (age >= 18 && hasLicence && !isDrunk) ? 'может' : 'не может';
+console.log(canDrive);
